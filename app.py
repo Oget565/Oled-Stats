fe64f60f4aa918fb115fff5946c353cbe67a01d0
oget565 <149737437+Oget565@users.noreply.github.com>
2025-10-08 21:45:17 -0400

    add: LAN ip section

--- app.py
+++ app.py
@@ -2,19 +2,48 @@ import psutil
 import socket
 import time
 
-
 def get_ip():
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    return ip_address
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+
+        if ip == "127.0.0.1":
+            return "N/A"
+
+    except Exception:
+        ip = "N/A"
+    finally:
+        s.close()
+    return ip
+
+def get_temp():
+    try:
+        temps = psutil.sensors_temperatures()
+        
+        if 'coretemp' in temps:
+            for entry in temps['coretemp']:
+                if 'Package id 0' in entry.label:
+                    return round(entry.current, 1)
+        
+        for name, entries in temps.items():
+            if entries:
+                return round(entries[0].current, 1)
+        
+        return None
+    except Exception as e:
+        print(f"Error reading temperature: {e}")
+        return None
 
 def main():
     while True:
         cpu = psutil.cpu_percent()
         ram_per = psutil.virtual_memory().percent
         disk = psutil.disk_usage("/").percent
+        temp = get_temp()
+        ip = get_ip()
         
-        print(f"Cpu: {cpu}, Ram: {ram_per}, Disk {disk}, IP: {get_ip()}")
+        print(f"Cpu: {cpu}%, Ram: {ram_per}%, Disk {disk}%, IP: {ip}, Temp: {temp}C")
         time.sleep(1)
 
 main()    
